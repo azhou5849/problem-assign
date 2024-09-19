070668bda7b2f8793faa2e989edc1135aab66201
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2024-09-19 07:50:58 -0400

    complete

--- assign.py
+++ assign.py
@@ -1,16 +1,10 @@
 # Imports and helpers
-from sys import exit
-from random import shuffle
-
-def proceed():
-    c = input("Do you wish to proceed? (Y/N) ")
-    if c == 'N' or c == 'n':
-        exit(1)
+import networkx as nx
 
 
 
 """
-Step 1: Get input data. There are n students and n problems, and each student will be assigned m problems so that every problem is assigned to m students.
+Step 1: Get input data.
 """
 print("-" * 80)
 print("STEP 1: INPUT DATA")
@@ -19,26 +13,13 @@ print("-" * 80)
 problems_string = input("Enter a comma-separated list of the problems that will be assigned: ")
 problems = [int(p) for p in problems_string.split(',')]
 n = len(problems)
-print(f"You have provided a list of {n} problems:", problems)
-proceed()
-
-print("-" * 80)
 
 students = []
 for i in range(n):   
     name = input(f"Enter the name of student {i}: ")
     students.append(name)
-shuffle(students)
-print(f"There are {n} students in the class, named:")
-for i in range(n):
-    print(f"{i:>2}. {students[i]}")
-proceed()
-
-print("-" * 80)
 
 m = int(input("How many problems should each student get? "))
-print(f"Each student should receive {m} problems.")
-proceed()
 
 
 
@@ -51,66 +32,28 @@ print("-" * 80)
 
 done = False
 while not done:
-    problems_per_student = {s: [] for s in students}
-    students_per_problem = {p: [] for p in problems}
-    done = True  # set to False when a duplicate assignment occurs
+    assignment_graph = nx.bipartite.configuration_model([m] * n, [m] * n, create_using = nx.Graph())
+    if assignment_graph.number_of_edges() == m * n:
+        done = True
 
-    problem_copies = problems * m
-    shuffle(problem_copies)
-    for s in students:
-        provisional_list = []
-        for _ in range(m):
-            p = problem_copies.pop()
-            provisional_list.append(p)
-            students_per_problem[p].append(s)
-        print(f"Problems for {s}: ", provisional_list)
-        if len(provisional_list) != len(set(provisional_list)):
-            print("Duplicate detected! Restarting...\n")
-            done = False
-            break
-        else:
-            problems_per_student[s] = sorted(provisional_list)
+nx.relabel_nodes(assignment_graph, {i: students[i] for i in range(n)}, copy = False)
+nx.relabel_nodes(assignment_graph, {j+n: problems[j] for j in range(n)}, copy = False)
 
-
-# Display problem assignments
-print("-" * 80)
-print("Students assigned to each problem:")
-for p in problems:
-    print(f"{p:>2}: {students_per_problem[p]}")
-print("Problems assigned to each student:")
-for s in students:
-    print(f"{s:<15} {problems_per_student[s]}")
-proceed()
+assignments = {s: sorted([p for p in assignment_graph.adj[s].keys()]) for s in students}
+max_name_length = max([len(s) for s in students])
+for student, problem_list in assignments.items():
+    print(f"{student:<{max_name_length}} {problem_list}")
 
 
 
 """
-Step 3: Pick one problem from each student to grade, so that every problem is graded exactly once. This is done with 
+Step 3: Pick problems to grade. This is done with the networkx matching function for bipartite graphs.
 """
 print("-" * 80)
 print("STEP 3: PICKING PROBLEMS TO GRADE")
 print("-" * 80)
 
-# Initialise
-unpaired_students = list(range(n))
-selections = {problem: None for problem in range(n)}
+grading = {s: nx.bipartite.maximum_matching(assignment_graph)[s] for s in students}
 
-# Iterate the offer-and-accept process
-rounds = {student: 0 for student in range(n)}
-while len(unpaired_students) > 0:
-    print(unpaired_students)
-    claimed_students = []
-    for student in unpaired_students:
-        problem = problems_per_student[student][rounds[student]]
-        rounds[student] += 1
-        if selections[problem] is None:
-            selections[problem] = student
-        else:
-            old_student = selections[problem]
-            if student < old_student:
-                selections[problem] = student
-    print(selections)
-    unpaired_students = [i for i in range(n) if i not in selections.values()]
-
-# Display final selection
-print(selections)
+for student, problem in grading.items():
+    print(f"{student:<{max_name_length}} {problem}")
